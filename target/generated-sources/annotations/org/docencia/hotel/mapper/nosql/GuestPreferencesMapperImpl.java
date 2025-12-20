3a784c8e47cbad73bd43f619f0262dea931be4e2
IsmaelJos <ismaelejano@gmail.com>
2025-12-20 13:28:44 +0000

    feat: :bookmark: Creacion y comprobacion
    Se ha creado el esqueleto del proyecto y se ha comprobado su funcionamiento esperado

---
+++ target/generated-sources/annotations/org/docencia/hotel/mapper/nosql/GuestPreferencesMapperImpl.java
@@ -0,0 +1,37 @@
+package org.docencia.hotel.mapper.nosql;
+
+import javax.annotation.processing.Generated;
+import org.docencia.hotel.domain.model.GuestPreferences;
+import org.docencia.hotel.persistence.nosql.document.GuestPreferencesDocument;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-12-20T13:25:02+0000",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.17 (Ubuntu)"
+)
+@Component
+public class GuestPreferencesMapperImpl implements GuestPreferencesMapper {
+
+    @Override
+    public GuestPreferencesDocument toDocument(GuestPreferences domain) {
+        if ( domain == null ) {
+            return null;
+        }
+
+        GuestPreferencesDocument guestPreferencesDocument = new GuestPreferencesDocument();
+
+        return guestPreferencesDocument;
+    }
+
+    @Override
+    public GuestPreferences toDomain(GuestPreferencesDocument doc) {
+        if ( doc == null ) {
+            return null;
+        }
+
+        GuestPreferences guestPreferences = new GuestPreferences();
+
+        return guestPreferences;
+    }
+}
